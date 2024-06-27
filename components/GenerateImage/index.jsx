a09b4ece9ad63c55c278d6bb5b7e478b1e1bad9c
kk0139317 <kk4881757@gmail.com>
2024-06-27 12:23:44 +0530

    modified:   components/Footer/index.jsx 	modified:   components/GenerateImage/index.jsx 	modified:   components/Header/index.jsx 	modified:   components/HeroSection/index.jsx 	modified:   components/Login/index.jsx 	modified:   components/LogoGrid/index.jsx 	modified:   components/Pricing/index.jsx 	modified:   components/Sidebar/index.jsx 	modified:   components/Signin/index.jsx

--- components/GenerateImage/index.jsx
+++ components/GenerateImage/index.jsx
@@ -1,5 +1,4 @@
 // components/GenerateImages.jsx
-'use client'
 import React, { useState, useEffect } from 'react';
 import useAuthRedirect from '@/utils/useAuthRedirect';
 import { useAuth } from '@/utils/auth';
@@ -8,7 +7,7 @@ import LoadingSpinner from 'react-loading'; // Example: import a loading spinner
 const GenerateImages = ({ prompt }) => {
   const [inputPrompt, setInputPrompt] = useState('');
   const [numImages, setNumImages] = useState(1);
-  const [images, setImages] = useState([]);
+  const [imageGroups, setImageGroups] = useState([]);
   const [loading, setLoading] = useState(false); // State for loading indicator
   const { token } = useAuthRedirect();
   const { username, logout } = useAuth();
@@ -29,12 +28,36 @@ const GenerateImages = ({ prompt }) => {
         return;
       }
       const data = await response.json();
-      setImages(data.images);
+      groupImagesBySubPrompt(data.images);
     } catch (error) {
       console.error('Error fetching images:', error);
     } finally {
       setLoading(false); // Set loading state to false after fetching
     }
+  };
+
+  const groupImagesBySubPrompt = (images) => {
+    // Group images by sub_prompt_id
+    const groupedImages = images.reduce((groups, image) => {
+      const key = image.sub_prompt_id;
+      if (!groups[key]) {
+        groups[key] = {
+          sub_prompt_id: image.sub_prompt_id,
+          sub_prompt_text: image.sub_prompt_text,
+          created_at: image.created_at,
+          images: [],
+        };
+      }
+      groups[key].images.push({
+        id: image.id,
+        url: image.url,
+      });
+      return groups;
+    }, {});
+
+    // Convert object to array of groups
+    const groupsArray = Object.values(groupedImages);
+    setImageGroups(groupsArray);
   };
 
   const handleGenerate = async (e) => {
@@ -58,7 +81,7 @@ const GenerateImages = ({ prompt }) => {
       }
 
       const data = await response.json();
-      setImages(data.images);
+      groupImagesBySubPrompt(data.images);
     } catch (error) {
       console.error('Error generating images:', error);
     } finally {
@@ -67,136 +90,87 @@ const GenerateImages = ({ prompt }) => {
   };
 
   return (
-    <div className="min-h-screen  flex flex-col h- items-center py-0 px-4 sm:px-6 lg:px-8">
-      {/* <header className="text-center mb-12"> */}
-        {/* <h1 className="text-5xl font-extrabold text-gray-900 mb-4 animate-fade-in-down">Image Generator</h1>
-        <p className="text-lg text-gray-700 animate-fade-in-up">Unleash your creativity with AI-powered image generation. Provide a prompt, and let the AI generate stunning images for you.</p> */}
-      {/* </header> */}
+    <div className="min-h-screen flex flex-col items-center py-0 px-4 sm:px-6 lg:px-8">
       {loading ? (
         <div className="w-full h-full flex items-center justify-center">
-        {/* //<LoadingSpinner type="spin" color="#4F46E5" height={80} width={80} /> Example: Loading spinner */}
-        
-<div role="status" className="max-w-sm p-4 border mx-4 border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
-    <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
-        <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
-            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
-            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
-        </svg>
-    </div>
-    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-    <div className="flex items-center mt-4">
-       <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
-        </svg>
-        <div>
-            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
-            <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-        </div>
-    </div>
-    <span className="sr-only">Loading...</span>
-</div>
-        
-<div role="status" className="max-w-sm p-4 mx-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
-    <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
-        <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
-            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
-            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
-        </svg>
-    </div>
-    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-    <div className="flex items-center mt-4">
-       <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
-        </svg>
-        <div>
-            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
-            <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-        </div>
-    </div>
-    <span className="sr-only">Loading...</span>
-</div>
-        
-<div role="status" className="max-w-sm p-4 mx-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
-    <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
-        <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
-            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
-            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
-        </svg>
-    </div>
-    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-    <div className="flex items-center mt-4">
-       <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
-        </svg>
-        <div>
-            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
-            <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-        </div>
-    </div>
-    <span className="sr-only">Loading...</span>
-</div>
+          {/* Display loading spinner or skeleton UI */}
+          <div role="status" className="max-w-sm p-4 border mx-4 border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
+            {/* Example loading spinner */}
+            <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+              <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+              </svg>
             </div>
-
-
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            <div className="flex items-center mt-4">
+              <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+              </svg>
+              <div>
+                <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
+                <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+              </div>
+            </div>
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
       ) : (
         <section className="w-full max-w-6xl mb-16 h-2/3 overflow-y-scroll absolute p-8">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 animate-fade-in-up">
-            {images.map((image, index) => (
-              <div key={image.id} className="relative group overflow-hidden rounded-lg shadow-lg transform transition duration-500 hover:scale-105">
-                <img className="w-full h-40 object-cover" src={`http://localhost:8000${image.url}`} alt={`Generated from prompt: ${image.prompt}`} />
-                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-                  {/* <p className="text-white text-lg font-bold text-center px-4">{image.prompt}</p> */}
-                </div>
+          {/* Render image groups */}
+          {imageGroups.map((group) => (
+            <div key={group.sub_prompt_id} className="mb-8">
+              <h2 className="text-2xl font-bold mb-4">{group.sub_prompt_text}</h2>
+              <p className="text-gray-500 mb-2">Created at: {new Date(group.created_at).toLocaleString()}</p>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {group.images.map((image, index) => (
+                  <div key={image.id} className="relative group overflow-hidden rounded-lg shadow-lg transform transition duration-500 hover:scale-105">
+                    <img className="w-full h-40 object-cover" src={`http://localhost:8000${image.url}`} alt={`Generated from prompt: ${group.sub_prompt_text}`} />
+                    <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
+                      {/* Optional overlay */}
+                    </div>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </section>
       )}
+      {/* Form for generating new images */}
       <section className="flex flex-col items-center w-full mb-0 animate-fade-in fixed bottom-0 p-4">
         <div className="bg-white rounded-lg shadow-2xl p-6 w-2/3">
-          <form onSubmit={handleGenerate} className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+          <form onSubmit={handleGenerate} className="grid grid-cols-1 sm:grid-cols-3 gap-4 items-center">
             <div className="sm:col-span-2">
               <input
                 type="text"
-                id="inputPrompt"
                 value={inputPrompt}
                 onChange={(e) => setInputPrompt(e.target.value)}
-                disabled={loading} // Disable input while loading
+                placeholder="Enter your prompt..."
+                className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-300"
                 required
-                className="shadow-md appearance-none border border-gray-300 rounded-lg w-full py-3 px-4 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-150 ease-in-out"
-                placeholder="Enter your prompt here...  e.g. 'a cute cat'"
               />
             </div>
             <div>
               <input
                 type="number"
-                id="numImages"
                 value={numImages}
                 onChange={(e) => setNumImages(e.target.value)}
+                placeholder="Num Images"
+                className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-300"
                 min="1"
-                placeholder="No of Images"
-                max="100"
+                max="10"
                 required
-                className="shadow-md appearance-none border border-gray-300 rounded-lg w-full py-3 px-4 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition duration-150 ease-in-out"
-                disabled={loading} // Disable input while loading
               />
             </div>
             <div className="sm:col-span-3">
               <button
                 type="submit"
-                disabled={loading} // Disable button while loading
-                className={`w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-3 px-4 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition duration-150 ease-in-out ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                className="w-full bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
               >
-                {loading ? 'Generating...' : 'Generate'}
+                Generate Images
               </button>
             </div>
           </form>
--- components/Sidebar/index.jsx
+++ components/Sidebar/index.jsx
@@ -30,6 +30,14 @@ const Sidebar = () => {
     setSelectedPrompt(prompt);
   };
 
+  const handleSubPromptSelect = (subPromptId) => {
+    // Find the selected subprompt based on subPromptId
+    const foundPrompt = prompts.find(prompt => prompt.sub_prompts.some(sub => sub.id === subPromptId));
+    if (foundPrompt) {
+      setSelectedPrompt(foundPrompt);
+    }
+  };
+
   return (
     <section className='' >
       <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200">
@@ -80,7 +88,7 @@ const Sidebar = () => {
             <span className="sr-only">Open user menu</span>
             <img
               className="w-8 h-8 rounded-full"
-              src="/docs/images/people/profile-picture-3.jpg"
+              src="https://img.freepik.com/free-vector/man-profile-account-picture_24908-81754.jpg?size=338&ext=jpg&ga=GA1.1.2116175301.1719273600&semt=ais_user"
               alt="user photo"
             />
           </button>
@@ -140,7 +148,7 @@ const Sidebar = () => {
     </div>
   </div>
 </nav>
-
+{/* 
 <aside
   id="logo-sidebar"
   className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 shadow-lg"
@@ -191,10 +199,52 @@ New Prompts
       ))}
     </ul>
   </div>
-</aside>
+</aside> */}
 
-
-
+<aside
+        id="logo-sidebar"
+        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 shadow-lg"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-4 pb-4 overflow-y-auto bg-white">
+          <ul className="space-y-4">
+            <a href="#">
+              <li className="flex justify-center items-center py-2 text-gray-600 text-xs uppercase font-semibold mb-2">
+                New Prompts
+              </li>
+            </a>
+            {prompts.map((prompt, index) => (
+              <li key={prompt.id} className={`group flex flex-col items-start px-4 py-3 rounded-lg cursor-pointer hover:bg-gray-100 transition-colors duration-300 ${index === 0 ? 'border-t border-gray-200' : ''}`}>
+                <button
+                  onClick={() => handlePromptClick(prompt)}
+                  className="flex items-center space-x-3 w-full text-base font-medium text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                >
+                  <div className="flex-shrink-0 w-8 h-8 flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500">
+                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                    </svg>
+                  </div>
+                  <span className="truncate text-sm ">{prompt.unique_id}</span>
+                </button>
+                <div className="flex w-full mt-1">
+                  <small className="text-xs font-light text-gray-500">{new Date(prompt.created_at).toLocaleString([], { dateStyle: 'short', timeStyle: 'short' })}</small>
+                </div>
+                <select
+                  className="mt-2 w-full border rounded-md py-1 px-2"
+                  onChange={(e) => handleSubPromptSelect(parseInt(e.target.value))}
+                >
+                  <option value="">Select a subprompt</option>
+                  {prompt.sub_prompts.map((subPrompt) => (
+                    <option key={subPrompt.id} value={subPrompt.id}>
+                      {subPrompt.prompt_text}
+                    </option>
+                  ))}
+                </select>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </aside>
 
       <div className="p-4 sm:ml-64 mt-10 h-100 overflow-hidden transition duration-300">
         <GenerateImages prompt={selectedPrompt} />
